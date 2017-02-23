912bdcf4d20e5218d47a02a5cb3f8939094f70f7
Maxim Beseda <maxim.beseda@gmail.com>
2017-02-23 23:09:28 +0200

    vers 2.1

--- src/concatenatedwords/ConcatenatedWordsSearch.java
+++ src/concatenatedwords/ConcatenatedWordsSearch.java
@@ -25,62 +25,31 @@ public class ConcatenatedWordsSearch implements Callable<ArrayList<String>> {
 
         for (int i = beginList; i < endList; i++) {
             System.out.println(i);
-            ArrayList<String> tempList = new ArrayList<>();
+            ArrayList<String> containsWords = new ArrayList<>();
             String conWord = wordsList.get(i);
             for (int j = 0; j < wordsList.size(); j++) {
                 if (!wordsList.get(j).equals(conWord) && conWord.contains(wordsList.get(j)) && (conWord.length() - wordsList.get(j).length()) != 1) {
-                    tempList.add(wordsList.get(j));
+                    containsWords.add(wordsList.get(j));
                 }
             }
 
+            ArrayList<String> tempList = new ArrayList<>(containsWords);
             tempList.sort(((o1, o2) -> o2.length() - o1.length()));
+            if (search(i, conWord, tempList)) continue;
 
-            for (int j = 0; j < tempList.size(); j++) {
-                conWord = conWord.replaceAll(tempList.get(j), "*");
-            }
-
-            if (!conWord.matches("(?i).*[a-zа-я].*")) {
-                concatenatedWordsList.add(wordsList.get(i));
-                continue;
-            }
-            conWord = wordsList.get(i);
-
+            tempList = new ArrayList<>(containsWords);
             tempList.sort(((o1, o2) -> o1.length() - o2.length()));
+            if (search(i, conWord, tempList)) continue;
 
-            for (int j = 0; j < tempList.size(); j++) {
-                conWord = conWord.replaceAll(tempList.get(j), "*");
-            }
-
-            if (!conWord.matches("(?i).*[a-zа-я].*")) {
-                concatenatedWordsList.add(wordsList.get(i));
-                continue;
-            }
-            conWord = wordsList.get(i);
-
+            tempList = new ArrayList<>(containsWords);
             tempList.sort((Comparator.naturalOrder()));
+            if (search(i, conWord, tempList)) continue;
 
-            for (int j = 0; j < tempList.size(); j++) {
-                conWord = conWord.replaceAll(tempList.get(j), "*");
-            }
-
-            if (!conWord.matches("(?i).*[a-zа-я].*")) {
-                concatenatedWordsList.add(wordsList.get(i));
-                continue;
-            }
-            conWord = wordsList.get(i);
-
+            tempList = new ArrayList<>(containsWords);
             tempList.sort((Comparator.reverseOrder()));
+            if (search(i, conWord, tempList)) continue;
 
-            for (int j = 0; j < tempList.size(); j++) {
-                conWord = conWord.replaceAll(tempList.get(j), "*");
-            }
-
-            if (!conWord.matches("(?i).*[a-zа-я].*")) {
-                concatenatedWordsList.add(wordsList.get(i));
-                continue;
-            }
-            conWord = wordsList.get(i);
-
+            tempList = new ArrayList<>(containsWords);
             tempList.sort(((o1, o2) -> {
                 if (o1.length() > o2.length()) {
                     return -1;
@@ -90,17 +59,9 @@ public class ConcatenatedWordsSearch implements Callable<ArrayList<String>> {
                 }
                 return o2.compareTo(o1);
             }));
+            if (search(i, conWord, tempList)) continue;
 
-            for (int j = 0; j < tempList.size(); j++) {
-                conWord = conWord.replaceAll(tempList.get(j), "*");
-            }
-
-            if (!conWord.matches("(?i).*[a-zа-я].*")) {
-                concatenatedWordsList.add(wordsList.get(i));
-                continue;
-            }
-            conWord = wordsList.get(i);
-
+            tempList = new ArrayList<>(containsWords);
             tempList.sort(((o1, o2) -> {
                 if (o1.length() > o2.length()) {
                     return 1;
@@ -110,15 +71,28 @@ public class ConcatenatedWordsSearch implements Callable<ArrayList<String>> {
                 }
                 return o2.compareTo(o1);
             }));
+            if (search(i, conWord, tempList)) continue;
 
-            for (int j = 0; j < tempList.size(); j++) {
-                conWord = conWord.replaceAll(tempList.get(j), "*");
-            }
-
-            if (!conWord.matches("(?i).*[a-zа-я].*")) {
-                concatenatedWordsList.add(wordsList.get(i));
-            }
         }
         return concatenatedWordsList;
+    }
+
+    private boolean search(int i, String conWord, ArrayList<String> tempList) {
+        for (int j = 0; j < tempList.size(); j++) {
+            for(int k = 0; k < tempList.size(); k++){
+                conWord = conWord.replaceAll(tempList.get(k), "*");
+            }
+
+            if (!conWord.matches("(?i).*[a-zа-я].*")) {
+                concatenatedWordsList.add(wordsList.get(i));
+                return true;
+            } else {
+                String a = tempList.get(0);
+                tempList.remove(0);
+                tempList.add(a);
+                conWord = wordsList.get(i);
+            }
+        }
+        return false;
     }
 }
